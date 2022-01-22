6658a3a8d74107e168478d2181e04c51e19fcabd
Johnny Vazquez <jvazquez@skylinenet.net>
2022-01-22 15:27:54 -0600

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from 'react'
+import ReactDOM from "react-dom"
+
+
+const root = document.getElementById('root')
+
+ReactDOM.render(<h1>Hello From TS React</h1> ,root)
